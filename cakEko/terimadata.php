1615de558ed42be1838c0023774304dd2153ba6c
epiii <gue.elfrianto@gmail.com>
2016-09-29 00:22:15 +0700

    ok

--- cakEko/terimadata.php
+++ cakEko/terimadata.php
@@ -10,7 +10,10 @@
 	if($_GET['mode']=='kirim'){ // insert 
 		$sql='INSERT INTO  tb_anu set val="'.$_GET['value'].'"';
 		$exe=mysqli_query($sql);
-		$out=array('msg'=>'send_mode',($exe?'insert_successfuly':'insert_error'));
+		$out=array(
+				'mode' =>'send_mode',
+				'msg'  =>($exe?'insert_successfuly':'insert_error')
+			);
 	}elseif($_GET['mode']=='terimaAll'){ //read all
 		$sql='SELECT * from tb_anu ';
 		$exe=mysqli_query($sql);
@@ -21,16 +24,21 @@
 				'val'       =>$rec['val'],
 				'timestamp' =>$rec['timestamp'],
 			);	
-		}$out=array('msg'=>'get_mode','data'=>$dtArr);
+		}$out=array(
+				'mode' =>'get_all_mode',
+				'msg'  =>($exe?'get_all_successfuly':'couldnot_get_all'),
+				'data' =>$dtArr
+			);
 	}elseif($_GET['mode']=='terimaCur'){ //read current (last or new data)
 		$sql     ='SELECT * from tb_anu LIMIT 1 order by desc';
 		$exe     =mysqli_query($sql);
 		$rec     =mysqli_fetch_assoc($exe);
 		$out =array(
-			'msg'       =>'get_mode',
-			'id'        =>$rec['id'],
-			'val'       =>$rec['val'],
-			'timestamp' =>$rec['timestamp'],
+			'mode' =>'get_current_mode',
+			'msg'  =>($exe?'get_current_successfuly':'couldnot_get_all'),
+			'id'   =>$rec['id'],
+			'val'  =>$rec['val'],
+			'timestamp' =>$rec['timestamp']
 		);
 	}else $out=array('msg'=>'invalid_parameter');
 
